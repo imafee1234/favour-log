72110e05bc451cda1709ccbd4277eb4ab94c7df3
imafee1234 <123467269+imafee1234@users.noreply.github.com>
2023-02-20 14:35:26 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,101 @@
+import React, { useState } from "react";
+
+import "./App.css";
+
+alert("you must be a lecture or student?");
+
+
+function App() {
+  // React States
+  const [errorMessages, setErrorMessages] = useState({});
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  // User Login info
+  const database = [
+    {
+      username: "user1",
+      password: "pass1"
+    },
+    {
+      username: "phillip",
+      password: "pass2"
+    }
+  ];
+
+  const errors = {
+    uname: "invalid username",
+    pass: "invalid password"
+  };
+
+  const handleSubmit = (event) => {
+    //Prevent page reload
+    event.preventDefault();
+
+    var { uname, pass } = document.forms[0];
+
+    // Find user login info
+    const userData = database.find((user) => user.username === uname.value);
+
+    // Compare user info
+    if (userData) {
+      if (userData.password !== pass.value) {
+        // Invalid password
+        setErrorMessages({ name: "pass", message: errors.pass });
+      } else {
+        setIsSubmitted(true);
+      }
+    } else {
+      // Username not found
+      setErrorMessages({ name: "uname", message: errors.uname });
+    }
+  };
+
+  // Generate JSX code for error message
+  const renderErrorMessage = (name) =>
+    name === errorMessages.name && (
+      <div className="error">{errorMessages.message}</div>
+    );
+
+  // JSX code for login form
+  const renderForm = (
+    <div className="form">
+      <form onSubmit={handleSubmit}>
+        <div className="input-container">
+          <label>Username </label>
+          <input type="text" name="uname" required />
+          {renderErrorMessage("uname")}
+        </div>
+        <div className="input-container">
+          <label>Email </label>
+          <input type="email" name="email" required />
+          {renderErrorMessage("email")}
+          </div>
+          <div className="input-container">
+          <label>password </label>
+          <input type="password" name="pass" required />
+          {renderErrorMessage("uname")}
+          </div>
+        <div className="input-container">
+          <label>confirm Password </label>
+          <input type="password" name="cpass" required />
+          {renderErrorMessage("pass")}
+        </div>
+        <div className="button-container">
+          <input type="submit" />
+        </div>
+      </form>
+    </div>
+  );
+
+  return (
+    <div className="app">
+      <div className="login-form">
+        <div className="title"><h2>LOG IN</h2></div>
+        {isSubmitted ? <div className="ET">
+          <h3>Welcome to Lincoln</h3></div> : renderForm}
+      </div>
+    </div>
+  );
+}
+
+export default App;
